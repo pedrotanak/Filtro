92aa24924531db6c039375c75607ef74ba50f214
pedrotanak <106190056+pedrotanak@users.noreply.github.com>
2023-01-19 16:40:38 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+x=0;
+y=0;
+function preload(){
+    guardaImagem=loadImage("https://i.postimg.cc/7ZBcjDqp/clownnose.png");
+}
+function setup(){
+    canvas=createCanvas(300,300);
+    canvas.position(800,300);
+
+}
